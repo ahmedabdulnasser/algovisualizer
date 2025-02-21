2ff5719684d03d37428a6063f95dc36a7a69da67
0xPOP <112898971+0xPOP@users.noreply.github.com>
2025-02-21 15:24:10 +0200

    astar: v3

--- js/a_star/index.js
+++ js/a_star/index.js
@@ -29,27 +29,28 @@
 class Main {
     constructor() {
         this.grid = document.getElementById('grid');
+        this.size = 20;
         this.squareList = [];
-        this.start = undefined;
-        this.end = undefined;
+        this.start = null;
+        this.end = null;
         this.paintComponent();
         this.isMouseDown = false;
         this.grid.addEventListener('mouseup', () => this.isMouseDown = false);
         this.grid.addEventListener('mouseleave', () => this.isMouseDown = false);
-        window.addEventListener('resize', 
-            () => this.paintComponent()
-        );
+        // window.addEventListener('resize', 
+        //     () => this.paintComponent()
+        // );
     }
 
     paintComponent() {
-        const girds_parent = this.grid.parentNode;
-        const size = 20; 
+        const gridParent = this.grid.parentNode;
 
-        const numCols = Math.floor(girds_parent.offsetWidth / size) - 1;
-        const numRows = Math.floor(girds_parent.offsetHeight / size) -1;
+        const numCols = Math.floor(gridParent.offsetWidth / this.size);
+        const numRows = Math.floor(gridParent.offsetHeight / this.size);
 
         this.grid.style.setProperty('--num-cols', numCols);
         this.grid.style.setProperty('--num-rows', numRows);
+        this.grid.style.setProperty('--size', this.size + 'px');
 
         this.squareList = [];
         this.grid.innerHTML = '';
@@ -57,43 +58,39 @@ class Main {
         for (let i = 0; i < numRows; i++) {
             for (let j = 0; j < numCols; j++) {
                 const square = document.createElement('div');
-                square.style.width = size + 'px';
-                square.style.height = size + 'px';
+                square.style.width = `${this.size}px`;
+                square.style.height = `${this.size}px`;
                 square.dataset.clicked = "false";
                 square.classList.add('square');
                 this.grid.appendChild(square);
-                square.addEventListener('click',function() {
-                    if (square.dataset.clicked === "false") {
-                        square.style.backgroundColor = 'black';
-                        square.dataset.clicked = "true";
-                    } else {
-                        square.style.backgroundColor = 'white';
-                        square.dataset.clicked = "false";
-                    }
-                });
-                square.addEventListener('mousedown', () => {
+
+                square.addEventListener('mousedown', (e) => {
                     this.isMouseDown = true;
-                    this.toggleSquareColor(square);
-                });
-
-                square.addEventListener('mouseover', () => {
-                    if (this.isMouseDown) {
+                    if (e.button === 0) {
                         this.toggleSquareColor(square);
+                    } else if (e.button === 2) {
+                        this.toggleSquareColor(square, "white");
                     }
                 });
 
-                square.addEventListener('click', () => {
-                    this.toggleSquareColor(square);
+                square.addEventListener('mouseover', (e) => {
+                    if (this.isMouseDown) {
+                        if (e.buttons === 1) {
+                            this.toggleSquareColor(square);
+                        } else if (e.buttons === 2) {
+                            this.toggleSquareColor(square, "white");
+                        }
+                    }
                 });
+
                 this.squareList.push(square);
             }
         }
     }
 
-    toggleSquareColor(square, from_color="white" ,to_color="black") {
-        const isClicked = square.dataset.clicked === "true";
-        square.style.backgroundColor = isClicked ? from_color : to_color;
-        square.dataset.clicked = !isClicked;
+    toggleSquareColor(square, color="black") {
+        square.style.backgroundColor = color;
+        square.dataset.clicked = square.dataset.clicked === "true" ? "false" : "true";
     }
 }
 
