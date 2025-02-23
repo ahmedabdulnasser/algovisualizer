ce67af884246dccec917b49b24be9b07841d472e
0xPOP <112898971+0xPOP@users.noreply.github.com>
2025-02-24 01:44:54 +0200

    astar: controls

--- js/a_star/index.js
+++ js/a_star/index.js
@@ -35,8 +35,12 @@ class Main {
         this.end = null;
         this.paintComponent();
         this.isMouseDown = false;
+        this.isSKeyPressed = false;
+        this.isEKeyPressed = false;
         this.grid.addEventListener('mouseup', () => this.isMouseDown = false);
         this.grid.addEventListener('mouseleave', () => this.isMouseDown = false);
+        document.addEventListener('keydown', (e) => this.handleKeyDown(e));
+        document.addEventListener('keyup', (e) => this.handleKeyUp(e));
         // window.addEventListener('resize', 
         //     () => this.paintComponent()
         // );
@@ -55,8 +59,8 @@ class Main {
         this.squareList = [];
         this.grid.innerHTML = '';
 
-        for (let i = 0; i < 5; i++) {
-            for (let j = 0; j < 1; j++) {
+        for (let i = 0; i < numCols; i++) {
+            for (let j = 0; j < numRows; j++) {
                 const square = document.createElement('div');
                 square.style.width = `${this.size}px`;
                 square.style.height = `${this.size}px`;
@@ -64,25 +68,41 @@ class Main {
                 square.dataset.Hcost = 0;
                 square.dataset.Gcost = 0;
                 square.dataset.Fcost = 0;
+                square.dataset.type = "none";
                 square.classList.add('square');
                 this.grid.appendChild(square);
-                this.drawCosts(square);
 
                 square.addEventListener('mousedown', (e) => {
                     this.isMouseDown = true;
                     if (e.button === 0) {
-                        this.toggleSquareColor(square);
+                        if (this.isSKeyPressed) {
+                            this.toggleStartNode(square);
+                        } else if (this.isEKeyPressed) {
+                            this.toggleEndNode(square);
+                        } else if(square !== this.start && square !== this.end) {
+                            this.toggleSquareColor(square);
+                        }
                     } else if (e.button === 2) {
-                        this.toggleSquareColor(square, "white");
+                        if (square !== this.start || square !== this.end) {
+                            this.toggleSquareColor(square, "white");
+                        }
                     }
                 });
 
                 square.addEventListener('mouseover', (e) => {
                     if (this.isMouseDown) {
                         if (e.buttons === 1) {
-                            this.toggleSquareColor(square);
+                            if (this.isSKeyPressed) {
+                                this.toggleStartNode(square);
+                            } else if (this.isEKeyPressed) {
+                                this.toggleEndNode(square);
+                            } else if (square !== this.start && square !== this.end) {
+                                this.toggleSquareColor(square);
+                            }
                         } else if (e.buttons === 2) {
-                            this.toggleSquareColor(square, "white");
+                            if (square !== this.start || square !== this.end) {
+                                this.toggleSquareColor(square, "white");
+                            }
                         }
                     }
                 });
@@ -92,7 +112,7 @@ class Main {
         }
     }
 
-    toggleSquareColor(square, color="black") {
+    toggleSquareColor(square, color = "black") {
         square.style.backgroundColor = color;
         square.dataset.clicked = square.dataset.clicked === "true" ? "false" : "true";
     }
@@ -112,6 +132,54 @@ class Main {
         Fcost.classList.add('fcost');
         Fcost.textContent = square.dataset.Fcost;
         square.appendChild(Fcost);
+    }
+
+    handleKeyDown(e) {
+        if (e.code === 'KeyS') {
+            this.isSKeyPressed = true;
+        } else if (e.code === 'KeyE') {
+            this.isEKeyPressed = true;
+        }
+    }
+
+    handleKeyUp(e) {
+        if (e.code === 'KeyS') {
+            this.isSKeyPressed = false;
+        } else if (e.code === 'KeyE') {
+            this.isEKeyPressed = false;
+        }
+    }
+
+    toggleStartNode(square) {
+        if (this.start === square) {
+            square.dataset.type = "none";
+            square.style.backgroundColor = "white";
+            this.start = null;
+        } else {
+            if (this.start) {
+                this.start.dataset.type = "none";
+                this.start.style.backgroundColor = "white";
+            }
+            square.dataset.type = "start";
+            square.style.backgroundColor = "green";
+            this.start = square;
+        }
+    }
+
+    toggleEndNode(square) {
+        if (this.end === square) {
+            square.dataset.type = "none";
+            square.style.backgroundColor = "white";
+            this.end = null;
+        } else {
+            if (this.end) {
+                this.end.dataset.type = "none";
+                this.end.style.backgroundColor = "white";
+            }
+            square.dataset.type = "end";
+            square.style.backgroundColor = "red";
+            this.end = square;
+        }
     }
 }
 
