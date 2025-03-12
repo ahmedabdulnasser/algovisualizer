d6a963c1f585a86c8a7102c260c7f7bb8bb9d732
Ahmed Abdel-Nasser <ahmedabdulnasser.ai@gmail.com>
2025-03-12 04:53:59 +0200

    Fix bugs in Sieve of Eratosthenes & Apply UI

--- array/sieve/sieve.js
+++ array/sieve/sieve.js
@@ -1,100 +1,116 @@
-var parent = document.getElementById('parent');
+var parent = document.getElementById("parent");
 var divs = [];
 var cells;
 var primes;
-var m = 400;
+var delayTime = 400;
 var pause = 0;
-var logs = document.getElementById('logs');
-function resume()
-{
-    pause = 0;
+var logs = document.getElementById("logs");
+let speeds = { x1: 750, x2: 500, x3: 200, x99: 0 };
+let playBtn = document.getElementById("playBtn");
+let pauseBtn = document.getElementById("pauseBtn");
+let renderBtn = document.getElementById("render-btn");
+let incSpeedBtn = document.getElementById("incSpeed");
+let decSpeedBtn = document.getElementById("decSpeed");
+let currSpeed = document.getElementById("sort-speed");
+currSpeed.value = "x1";
+currSpeed.dataset.speed = 750;
+
+playBtn.addEventListener("click", () => {
+  pause = 0;
+});
+pauseBtn.addEventListener("click", () => {
+  pause = 1;
+});
+renderBtn.addEventListener("click", play);
+incSpeedBtn.addEventListener("click", speedup);
+decSpeedBtn.addEventListener("click", speeddown);
+
+function play() {
+  primes = document.getElementById("primes_area");
+  numbersArea = document.getElementById("parent");
+  cells = document.getElementsByClassName("cell");
+  //   containerSection = document.getElementById("col2");
+  //   containerSection.style.height = "100vh";
+  primes.innerHTML = numbersArea.innerHTML = "";
+  console.log(primes);
+  let N = parseInt(document.getElementById("size").value);
+  if (isFinite(N) == false || !N) return;
+  //   var form = document.getElementById("form");
+  //   form.style.display = "none";
+  drawGrid(N);
+  sieve(N);
 }
-function stop()
-{
-    pause = 1;
+function speedup() {
+  if (currSpeed.value === "x1") {
+    currSpeed.value = "x2";
+  } else if (currSpeed.value === "x2") {
+    currSpeed.value = "x3";
+  } else if ((currSpeed.value = "x3")) {
+    currSpeed.value = "x99";
+  }
+  delayTime = currSpeed.dataset.speed = speeds[currSpeed.value];
 }
-function play()
-{
-    let N = parseInt(document.getElementById('size').value) ;
-    if (isFinite(N) == false || !N)return ;
-    var form = document.getElementById('form');
-    form.style.display='none';
-    drewGrid(N);
-    cells = document.getElementsByClassName('cell');
-    primes = document.getElementById('primes_area');
-    sieve(N);
+function speeddown() {
+  if (currSpeed.value === "x99") {
+    currSpeed.value = "x3";
+  } else if (currSpeed.value === "x3") {
+    currSpeed.value = "x2";
+  } else if (currSpeed.value === "x2") {
+    currSpeed.value = "x1";
+  }
+  delayTime = currSpeed.dataset.speed = speeds[currSpeed.value];
 }
-function speedup()
-{
-    m -= 20;
-    if (m < 1)m = 1;
+function max() {
+  delayTime = 0;
 }
-function speeddown()
-{
-    m += 20;
-    if (m > 1000)m = 1000;
-}
-function max()
-{
-    m = 0;
-}
-function drewGrid(n)
-{
-    for (let i = 1; i <=  n; i ++)
-    {
-        var div = document.createElement('div');
-        divs[i] = [];
-        div.classList.add('cell');
-        if (i > 1)
-        div.classList.add('prime');
-        div.innerHTML = i;
-        parent.appendChild(div);
-    }
+function drawGrid(n) {
+  for (let i = 1; i <= n; i++) {
+    var div = document.createElement("div");
+    divs[i] = [];
+    div.classList.add("cell");
+    if (i > 1) div.classList.add("prime");
+    div.innerHTML = i;
+    parent.appendChild(div);
+  }
 }
 async function delay(ms) {
-    while (pause)
-    {
-        await new Promise(resolve =>setTimeout(resolve, ms));
-    }
-    return new Promise(resolve => setTimeout(resolve, ms));
+  while (pause) {
+    await new Promise((resolve) => setTimeout(resolve, ms));
+  }
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
-async function sieve(n)
-{
-    cells[0].classList.add('not_prime');
-    for (let i = 2; i <= n; i ++)
-    {
-        cells[i - 1].classList.add('select-outer');
-        cells[i - 2].classList.remove('select-outer');
-        if (cells[i - 1].classList.contains('prime'))
-        {
-            logs.innerHTML += `${i} is prime !`;
-            logs.innerHTML += "<br>";
-            logs.innerHTML += `mark multiples of ${i} as not primes .....`;
-            logs.innerHTML += "<br>";
-            
-            let d = document.createElement('div');
-            d.innerHTML=i;
-            divs[i] = [i];
-            primes.appendChild(d);
-            for (let j = i + i; j <= n; j += i)
-            {
-                let li = document.createElement('li');
-                if (divs[j])divs[j].push(i);
-                else divs[j ] = [i];
-                cells[j - 1].classList.remove('prime');
-                cells[j - 1].classList.add('not_prime');
-                cells[j - 1].classList.add('select');
-                await delay(m);
-                cells[j - 1].classList.remove('select');
-            }
-        }
-        else await delay(m);
-        
-    }
-}
-function closeLogs(){
-logs.style.display = "none";
-var cbtn = document.getElementById('cbtn');
-cbtn.style.display = "none";
-}
+async function sieve(n) {
+  cells[0].classList.add("not_prime");
+  logs.innerHTML = "";
+  for (let i = 2; i <= n; i++) {
+    cells[i - 1].classList.add("select-outer");
+    cells[i - 2].classList.remove("select-outer");
+    if (cells[i - 1].classList.contains("prime")) {
+      logs.innerHTML += `<span style="color: red;">${i} is prime!</span>`;
+      logs.innerHTML += "<br>";
+      logs.innerHTML += `Marking multiples of ${i} as not prime.....`;
+      logs.innerHTML += "<br>";
+      let d = document.createElement("div");
+      d.innerHTML = i;
+      divs[i] = [i];
+      d.classList.add("primeElement");
 
+      primes.appendChild(d);
+      for (let j = i + i; j <= n; j += i) {
+        let li = document.createElement("li");
+        if (divs[j]) divs[j].push(i);
+        else divs[j] = [i];
+        cells[j - 1].classList.remove("prime");
+        cells[j - 1].classList.add("not_prime");
+        cells[j - 1].classList.add("select");
+        await delay(delayTime);
+        cells[j - 1].classList.remove("select");
+      }
+    } else await delay(delayTime);
+  }
+}
+function closeLogs() {
+  logs.style.display = "none";
+  var cbtn = document.getElementById("cbtn");
+  cbtn.style.display = "none";
+}
